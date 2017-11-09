92105bfe07654c22c41462bc3ab85c3e9039f750
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-11-08 23:31:01 -0600

    Adding storm dependency

---
+++ src/main/java/storm/CallLogCreatorBolt.java
@@ -0,0 +1,48 @@
+package storm;
+
+import java.util.Map;
+
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+
+//import Storm IRichBolt package
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Tuple;
+
+/**
+ * Created by HarshPatil on 11/8/17.
+ */
+public class CallLogCreatorBolt implements IRichBolt {
+
+    //Create instance for OutputCollector which collects and emits tuples to produce output
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String from = tuple.getString(0);
+        String to = tuple.getString(1);
+        Integer duration = tuple.getInteger(2);
+        collector.emit(new Values(from + " - " + to, duration));
+    }
+
+    @Override
+    public void cleanup() {}
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("call", "duration"));
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+}
